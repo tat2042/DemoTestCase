8c788205c42fda11fcc91223313bd87a395a690a
tat2042 <darkness171717@gmail.com>
2024-03-13 15:59:05 +0700

    first commit

---
+++ TestDemoTests/BasicMathsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo.Tests
+{
+    [TestClass()]
+    public class BasicMathsTests
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(10, 10);
+            Assert.AreEqual(res, 20);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(res, 100);
+        }
+    }
+}
